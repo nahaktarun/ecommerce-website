fa75daf9e0c484dde1fc3b26f3bc6676be1b7437
Tarun Nahak <nahaktarun@Taruns-MacBook-Pro.local>
2025-01-12 22:42:00 +0530

    Added the products lists for redux store

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// 1. create a base url using axios and use different paths for different resources.
+const API = axios.create({
+    baseURL: "http://localhost:5001/api"
+})
+
+export default API
