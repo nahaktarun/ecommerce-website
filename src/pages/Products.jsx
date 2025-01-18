6e5df121183da156588b583c58a26a1fb77be902
nahaktarun <tarunnahak25@gmail.com>
2025-01-18 20:43:28 +0530

    Added products list

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -16,8 +16,18 @@ if (error) return <div className="container mx-auto"> Error :{error}</div>
 
     return (
       <div className="container mx-auto">
-        <h1 className="text-3xl font-bold mb-4">Welcome to the store</h1>
-        <p className="text-gray-700 mb-6">Product page!</p>
+       <h2 className="text-2xl font-bold mb-4">Products</h2>
+       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <div key={product._id} className="bg-white p-4 shadow rounded flex flex-col">
+            <img src={product.image} alt={product.title} className="h-48 object-cover mb-4 rounded"/>
+            <h3 className="text-lg font-bold">{product.title}</h3>
+            <p className="text-gray-600 my-2">{product.price}</p>
+            <p className="text-gray-500 mb-2">{product.description}</p>
+            <button className="mt-auto bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Add to cart</button>
+          </div>
+        ))}
+       </div>
       </div>
     );
   };
