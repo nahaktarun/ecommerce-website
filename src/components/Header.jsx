f8eeadd57251fc99c60f39296a83a621b8fffff9
nahaktarun <tarunnahak25@gmail.com>
2025-01-25 21:19:33 +0530

    added logout function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {logoutUser} from '../features/authSlice';
 const Header = () => {
+  const dispatch = useDispatch();
+  const {isAuthenticated, user} = useSelector((state)=> ({
+    isAuthenticated: !!state.auth.token,
+    user: state.auth.user,
+  }))
+  const handleLogout = ()=>{
+    dispatch(logoutUser());
+  }
   return (
     <header className="bg-white shadow">
       {/* Title */}
@@ -19,6 +29,29 @@ const Header = () => {
                 Products
               </Link>
             </li>
+            {!isAuthenticated ? (
+              <>
+              <li>
+              <Link to="/login" className="text-gray-600 hover:text-gray-800">
+                Login
+              </Link>
+              </li>
+              <li>
+              <Link to="/register" className="text-gray-600 hover:text-gray-800">
+                Register
+              </Link>
+              </li>
+              </>
+            ): (
+              <>
+              <li>
+                <span className="text-gray-600">Welcome, {user?.name || 'User'}</span>
+              </li>
+              <li>
+                <button onClick={handleLogout} className="text-gray-600 hover:text-red-600">Logout</button>
+              </li>
+              </>
+            )}
           </ul>
         </nav>
       </div>
